1b51ceb040a87abd52233843845658f374d9ab6e
Rawah <113656545+UzumakiRawah@users.noreply.github.com>
2023-03-16 02:04:46 +0200

    Create 102-rating_shows.sql

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- Records are ordered by descending rating.
+SELECT `title`, SUM(`rate`) AS `rating`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_ratings` AS r
+       ON t.`id` = r.`show_id`
+ GROUP BY `title`
+ ORDER BY `rating` DESC;
