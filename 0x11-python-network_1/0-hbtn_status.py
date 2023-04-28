2d5e8ba30dc152dfb8f511ad9e7ca64f00b27aa3
Rawah <113656545+UzumakiRawah@users.noreply.github.com>
2023-04-29 01:00:37 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""Python script Fetches https://intranet.hbtn.io/status."""
-if __name__ == "__main__":
-    import urllib.request
+"""Fetches https://intranet.hbtn.io/status"""
+import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
-            as response:
+
+def fetcher():
+    """fetcher"""
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
 
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
+if __name__ == "__main__":
+    fetcher()
